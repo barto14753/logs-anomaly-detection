5beefadd55eb151a44b265d500db72eb20f68c70
bartek <wlodarski_bartosz@o2.pl>
2024-03-11 00:01:33 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # logs-anomaly-detection
+
+[![](https://img.shields.io/badge/Python-FFD43B?style=for-the-badge&logo=python&logoColor=blue)]()
+[![](https://img.shields.io/badge/InfluxDB-22ADF6?style=for-the-badge&logo=InfluxDB&logoColor=white)]()
+
+## Task 1
+
+Prepare package of logs from application under cyber attack
+
+## Task 2
+
+Analyze logs to detect and classify cyber attack
--- logs_export.py
+++ logs_export.py
@@ -1,12 +1,17 @@
 import influxdb_client, os, time
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import Point
 from influxdb_client.client.write_api import SYNCHRONOUS
+import time
+from datetime import datetime, timezone
 
+# Constants
 logs_file = "logs.txt"
 bucket="bucket"
 token = os.environ.get("INFLUXDB_TOKEN")
 org = "org"
 url = "http://localhost:8086"
+
+# API
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
@@ -16,7 +21,15 @@ def convert_to_number(value):
         return number_value
     except ValueError:
         return value
-    
+
+def convert_time(value):
+    timestamp = int(time.time()) - 48 * 3600 + int(value)
+    dt_object = datetime.utcfromtimestamp(timestamp).replace(tzinfo=timezone.utc)
+    formatted_time = dt_object.strftime("%Y-%m-%dT%H:%M:%SZ")
+    print(formatted_time)
+    return formatted_time
+
+
 def main():
     with open(logs_file, 'r') as file:
         columns= None
@@ -27,8 +40,11 @@ def main():
                 data  = line.strip().split(",")
                 point = Point("logs")
                 for column, value in zip(columns, data):
-                    if column in ["Time", "Duration", "SrcPackets", "DstPackets", "SrcBytes", "DstBytes"]:
+                    if column in ["Duration", "SrcPackets", "DstPackets", "SrcBytes", "DstBytes"]:
                         value = convert_to_number(value)
+                    elif column == "Time":
+                        formatted_time = convert_time(value)
+                        point = point.time(formatted_time)
                     point = point.field(column, value)
                 print(point)
                 write_api.write(bucket=bucket, org="org", record=point)
